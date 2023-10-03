4c40b0fdfdc707c96b8a939feca475ddfc7c0d94
gramythedj <81061885+gramythedj@users.noreply.github.com>
2023-10-03 22:01:04 +0530

    newest commit

---
+++ javascript.js
@@ -0,0 +1,29 @@
+
+       const taskInput = document.getElementById("taskInput");
+       const taskList = document.getElementById("taskList");
+
+       // Function to add a new task
+       function addTask() {
+           const taskText = taskInput.value;
+           if (taskText.trim() === "") {
+               alert("Task cannot be empty!");
+               return;
+           }
+// creating a list of tasks
+           const taskItem = document.createElement("li");
+           taskItem.textContent = taskText;
+
+           // Add a delete button
+           const deleteButton = document.createElement("button");
+           deleteButton.textContent = "Delete";
+           deleteButton.onclick = function() {
+               taskItem.remove();
+           };
+
+           // Append the task item
+         
+           taskList.appendChild(taskItem);
+
+           // Clear the input field
+           taskInput.value = "";
+       }
